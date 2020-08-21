3d37bdc383373df90754f069c39f36a266385bc1
Markus <57456255+maacc86@users.noreply.github.com>
2020-08-21 19:33:55 +0200

    Initial commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <div
+    // löscht item aus array
+    //onClick={() => {
+    //props.onChecked(props.id);
+    //}}
+    >
+      <li
+        onClick={handleClick}
+        style={{ textDecoration: isDone ? "line-through" : "none" }}
+      >
+        {props.text}
+      </li>
+    </div>
+  );
+}
+
+export default ToDoItem;
